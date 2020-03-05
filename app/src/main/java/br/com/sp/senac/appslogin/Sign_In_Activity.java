b4c7737c0e0f31d28393950532123156154fef0f
bruno.bsilva32 <bruno.batista82@hotmail.com>
2020-03-05 12:17:10 -0300

    Initial commit

---
+++ app/src/main/java/br/com/sp/senac/appslogin/Sign_In_Activity.java
@@ -0,0 +1,14 @@
+package br.com.sp.senac.appslogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sign_In_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in_layout);
+    }
+}
